0d7d14ad402aaaed3704a5b89f2066ba99ccc594
Manjula-g475 <manjulag475@gmail.com>
2024-02-15 21:00:55 +0530

    git push
    Git Push

---
+++ Feb15/src/Test2/Example1.java
@@ -0,0 +1,13 @@
+package Test2;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=5;
+		System.out.println(x);
+
+	}
+
+}
---
+++ Feb15_Java/src/Test2/Example.java
@@ -0,0 +1,12 @@
+package Test2;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		System.out.println(x);
+
+	}
+
+}
