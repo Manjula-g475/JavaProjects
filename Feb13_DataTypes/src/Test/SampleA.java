0ee55b63e19a73cae21472f575baa978e759a0a6
Manjula-g475 <manjulag475@gmail.com>
2024-02-15 12:20:29 +0530

    Initial commit

---
+++ Feb13_DataTypes/src/Test/SampleA.java
@@ -0,0 +1,23 @@
+package Test;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 12;
+		short s = 20;
+		int i = 30;
+		long l = 125l;
+		double d = 2987d;
+		float f = 234.4f;
+		char ch = 'A';
+		boolean bl = true;
+		String str =" Hello world";
+		
+		System.out.println("Primitive datatypes are:" +b + " " +s + " "+i +" "+l + " "+d +" "+f +" "+ch +" "+bl);
+		System.out.println("Non primitive datatypes are:" +str);
+
+
+	}
+
+}
